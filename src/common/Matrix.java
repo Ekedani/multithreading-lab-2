fd31d0623f200fbc8d4c0db2bd2a83ed390346d2
Vladlen <ilusenkovladlen@gmail.com>
2023-04-10 19:36:49 +0300

    Added some Matrix methods

--- src/common/Matrix.java
+++ src/common/Matrix.java
@@ -1,5 +1,56 @@
 package common;
 
-public class Matrix {
+import java.util.Random;
 
+public class Matrix {
+    private int numRows;
+    private int numCols;
+    public double[][] data;
+
+    public Matrix(int numRows, int numCols) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.data = new double[numRows][numCols];
+    }
+
+    public int getNumRows() {
+        return this.numRows;
+    }
+
+    public int getNumCols() {
+        return this.numCols;
+    }
+
+    protected void transpose() {
+        double[][] newData = new double[numCols][numRows];
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                newData[j][i] = data[i][j];
+            }
+        }
+        int temp = numRows;
+        numRows = numCols;
+        numCols = temp;
+        data = newData;
+    }
+
+    protected void generateRandomMatrix(double minVal, double maxVal) {
+        Random random = new Random();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                data[i][j] = minVal + (maxVal - minVal) * random.nextDouble();
+            }
+        }
+    }
+
+    protected void generateIdentityMatrix() {
+        if (numRows != numCols) {
+            throw new IllegalArgumentException("Matrix must be square to generate identity matrix.");
+        }
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                data[i][j] = i == j ? 1.0 : 0.0;
+            }
+        }
+    }
 }
---
+++ src/common/Result.java
@@ -0,0 +1,7 @@
+package common;
+
+public class Result extends Matrix {
+    public Result(int numRows, int numCols) {
+        super(numRows, numCols);
+    }
+}
